2aa1865b898b921f7a716e2059817f4c5480ebe9
ChristeenaKSunny <christeenakarunattu2000@gmail.com>
2024-02-05 18:16:14 +0300

    connecting the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connection to database sucessfull!");
+}).catch((err) => {
+    console.log(err);
+})
 
 app.listen("3000", () => {
     console.log("App running on port 3000!!");
